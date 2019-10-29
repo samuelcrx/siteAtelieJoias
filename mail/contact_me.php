fa80cb9f785572fcc71ba501b2ddd12b1bd5450f
Samuel Silva Braga <samuel.devsilva@gmail.com>
2019-10-29 11:19:58 -0300

    Site Atelie

---
+++ mail/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'samuel@cpanel.grupohdc.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "Você recebeu uma nova mensagem de seu website.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\Telefone: $phone\n\Mensagem:\n$message";
+$headers = "From: naoresponda@ssbsoftware.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
+?>
